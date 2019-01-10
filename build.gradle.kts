7221ba13d01d4a8f848f9d82dad1f346fc3f7efc
victor.petukhov <victor.petukhov@jetbrains.com>
2019-01-10 15:54:48 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.0-rc-146"
+}
+
+group = "org.jetbrains.kotlin"
+version = "0.1"
+
+repositories {
+    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("org.antlr:antlr4-runtime:4.7.1")
+    compile("org.antlr:ST4:4.0.8")
+    compile("org.antlr:antlr4:4.7.1")
+    compile("com.github.mifmif:generex:1.0.2")
+    compile("com.xenomachina:kotlin-argparser:2.0.4")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
