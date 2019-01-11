9b71b3926e52f863787c8a2e288f561b336f3629
victor.petukhov <victor.petukhov@jetbrains.com>
2019-01-11 21:21:45 +0300

    Use xml.bind.jdom

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ group = "org.jetbrains.kotlin"
 version = "0.1"
 
 repositories {
-    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
+    maven { setUrl("http://dl.bintray.com/jonnyzzz/maven") }
     mavenCentral()
     jcenter()
 }
@@ -18,8 +18,8 @@ dependencies {
     compile("org.antlr:antlr4-runtime:4.7.1")
     compile("org.antlr:ST4:4.0.8")
     compile("org.antlr:antlr4:4.7.1")
-    compile("com.github.mifmif:generex:1.0.2")
     compile("com.xenomachina:kotlin-argparser:2.0.4")
+    compile("org.jonnyzzz.kotlin.xml.bind:jdom:0.2.0")
 }
 
 tasks.withType<KotlinCompile> {
